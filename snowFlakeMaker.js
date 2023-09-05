de9dedf5ce183372bba41242e13452c518d0747b
Jackson <54775398+WasabiMushyPeas@users.noreply.github.com>
2023-09-05 15:06:24 -0600

    snow flake start

---
+++ snowFlakeMaker.js
@@ -0,0 +1,20 @@
+// Javascript program that procedurally generates snowflakes
+
+// Variables 
+var imageWidthAndHeight = 512;
+var imageCenter = imageWidthAndHeight / 2;
+
+// Snowflake Variables
+var snowFlakeSides = 8;
+var snowFlakeRadius = 200;
+var snowFlakeAngle = 0;
+var snowFlakeOffshootLength = 0;
+var snowFlakeOffshootAngle = 0;
+var snowFlakeOffshootAmount = 0;
+var snowFlakeThickness = 0;
+var snowFlakeColor = "#ffffff";
+
+
+// 2D Image Array
+
+
